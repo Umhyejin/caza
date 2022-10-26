d0ae7acf4f168a8515c83c21cbdd5e2d71f104f8
Umhyejin <aacc1177@naver.com>
2022-10-27 00:23:02 +0900

    5번 사진등록

--- app/src/main/java/com/example/caza/Activity_5.java
+++ app/src/main/java/com/example/caza/Activity_5.java
@@ -1,8 +1,22 @@
 package com.example.caza;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Activity_5 extends AppCompatActivity {
 
@@ -10,5 +24,49 @@ public class Activity_5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_5);
+
+        Button selectImageBtn = findViewById(R.id.selectImageBtn);
+        View imageView = findViewById(R.id.imageView);
+
+        selectImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityResult.launch(intent);
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> startActivityResult;
+
+    {
+        startActivityResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+
+                            Uri uri  = result.getData().getData();
+
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                                ImageView imageView = null;
+                                imageView.setImageBitmap(bitmap);
+
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
     }
 }
+
+
+
+
+
